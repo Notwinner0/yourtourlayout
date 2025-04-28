8cbabd970aad5974f81c0e65c0f80fc3ecb315ab
Notwinner <iliazukov61@gmail.com>
2025-04-28 20:59:58 +0300

    fixing pages day 1 :)

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const autoprefixer = require('autoprefixer');
-const PnpWebpackPlugin = require('pnp-webpack-plugin'); // Импорт плагина PnP
+const PnpWebpackPlugin = require('pnp-webpack-plugin');
 
 // Укажите здесь точное имя вашего репозитория на GitHub
 const REPOSITORY_NAME = 'yourtourlayout'; // !!! ЗАМЕНИТЕ НА ИМЯ ВАШЕГО РЕПОЗИТОРИЯ !!!
@@ -32,33 +32,31 @@ module.exports = (env, argv) => {
       port: 8080,
       open: true,
       hot: true,
-      historyApiFallback: true, // Полезно для одностраничных приложений
+      historyApiFallback: true,
     },
 
     resolve: {
       plugins: [
-        PnpWebpackPlugin, // Добавляем плагин PnP в список плагинов резолвера
+        PnpWebpackPlugin,
       ],
-      // Добавьте разрешение для расширений файлов, чтобы не писать их в import/require
       extensions: ['.js', '.json', '.css', '.scss'],
     },
 
     module: {
       rules: [
-        // Правило для JavaScript
         {
           test: /\.js$/,
-          // При использовании PnP, исключать node_modules обычно не требуется,
-          // но если есть проблемы, можно попробовать раскомментировать следующую строку:
-          // exclude: /node_modules/,
+          // exclude: /node_modules/, // При использовании PnP обычно не нужно исключать
           use: {
             loader: 'babel-loader',
             options: {
-              presets: [['@babel/preset-env', { useBuiltIns: 'usage', corejs: 3 }]], // Добавлено useBuiltIns для полифиллов
+              presets: [
+                // Использование useBuiltIns: 'usage' требует установки core-js как зависимости
+                ['@babel/preset-env', { useBuiltIns: 'usage', corejs: 3 }]
+              ],
             },
           },
         },
-        // Правило для SCSS и CSS
         {
           test: /\.(scss|css)$/,
           use: [
@@ -77,23 +75,19 @@ module.exports = (env, argv) => {
             'sass-loader',
           ],
         },
-        // Правило для изображений и шрифтов
         {
           test: /\.(png|svg|jpg|jpeg|gif|woff|woff2|eot|ttf|otf)$/,
           type: 'asset/resource',
           generator: {
-            // Убедитесь, что путь здесь не начинается со слэша, т.к. publicPath добавит его
             filename: isProduction ? 'assets/[hash][ext][query]' : 'assets/[name][ext][query]'
           }
         },
-        // Правило для HTML
         {
           test: /\.html$/,
           use: [
             {
               loader: 'html-loader',
               options: {
-                // Обрабатывать ссылки на изображения и т.д. в HTML
                 sources: {
                   list: [
                     {
@@ -105,15 +99,13 @@ module.exports = (env, argv) => {
                       tag: 'link',
                       attribute: 'href',
                       type: 'src',
-                      // Учитывать только теги link с rel="stylesheet"
                       filter: (tag, attribute, attributes) => {
                         return attributes.rel === 'stylesheet';
                       },
                     },
-                    // Добавьте другие теги/атрибуты, если необходимо
                   ],
                 },
-                minimize: isProduction, // Минифицировать HTML в production
+                minimize: isProduction,
               },
             },
           ],
@@ -124,33 +116,26 @@ module.exports = (env, argv) => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './src/index.html',
-        filename: 'index.html', // Выходное имя файла в папке dist
-        minify: isProduction, // Минифицировать HTML в production
+        filename: 'index.html',
+        minify: isProduction,
       }),
       new MiniCssExtractPlugin({
         filename: isProduction ? 'css/[name].[contenthash].css' : 'css/[name].bundle.css',
       }),
-      // CleanWebpackPlugin уже добавлен и настроен в output.clean: true
-      // new CleanWebpackPlugin(),
     ],
 
     optimization: {
       minimizer: [
         new CssMinimizerPlugin(),
-        // Добавьте TerserPlugin для минификации JS в production, если он не идет по умолчанию
-        // new TerserPlugin({
-        //   parallel: true,
-        // }),
       ],
       splitChunks: {
-         chunks: 'all', // Разделение кода для оптимизации загрузки
+         chunks: 'all',
       },
     },
 
-    // >>> Добавьте или обновите секцию resolveLoader
     resolveLoader: {
       plugins: [
-        PnpWebpackPlugin.moduleLoader(module), // Добавляем плагин PnP для резолва лоадеров
+        PnpWebpackPlugin.moduleLoader(module),
       ],
     },
   };
