d631415e3bb736b08de88fc3dc100a0e71587dcd
Notwinner <iliazukov61@gmail.com>
2025-04-29 21:52:43 +0300

    first section + header done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import './styles/main.scss'; // Импорт SCSS в JS (Webpack обработает)
 
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = '<p>JavaScript is working!</p>';
 
 console.log('JS and SCSS are linked!');
 
